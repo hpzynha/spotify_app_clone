2e7a162f7f0c11bf1b4cd1c831b2984d902cd55d
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-08-30 13:34:44 +0930

    signin or SignUp

--- lib/core/configs/theme/app_theme.dart
+++ lib/core/configs/theme/app_theme.dart
@@ -29,8 +29,9 @@ class AppTheme {
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
         backgroundColor: AppColors.primary,
+        elevation: 0,
         textStyle: const TextStyle(
-          fontSize: 20,
+          fontSize: 16,
           fontWeight: FontWeight.bold,
         ),
         shape: RoundedRectangleBorder(
---
+++ lib/presentation/auth/pages/signup_or_signinin.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_app_clone/common/appbar/app_bar.dart';
+import 'package:spotify_app_clone/common/helpers/is_dark_mode.dart';
+import 'package:spotify_app_clone/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_app_clone/core/configs/assets/app_images.dart';
+import 'package:spotify_app_clone/core/configs/assets/app_vectors.dart';
+import 'package:spotify_app_clone/core/configs/theme/app_colors.dart';
+
+class SignupOrSignininPage extends StatelessWidget {
+  const SignupOrSignininPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          const BasicAppBar(),
+          Align(
+            alignment: Alignment.topRight,
+            child: SvgPicture.asset(
+              AppVectors.topPattern,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: SvgPicture.asset(
+              AppVectors.bottomPattern,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: Image.asset(
+              AppImages.authBG,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AppVectors.logo,
+                  ),
+                  const SizedBox(height: 55),
+                  const Text(
+                    'Enjou Listening To Music',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 26,
+                    ),
+                  ),
+                  const SizedBox(height: 21),
+                  const Text(
+                    'Spotify is a proprietary Swedish audio streaming and media services provider',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 17,
+                      color: AppColors.grey,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  const SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: BasicAppButton(
+                          onPressed: () {},
+                          title: 'Register',
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Sign in',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                              color: context.isDarkMode
+                                  ? Colors.white
+                                  : Colors.black,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
