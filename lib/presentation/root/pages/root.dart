81cd60ada467ac4acf3328e4a97838ce986e5af8
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-09-22 06:59:55 +0930

    configs

--- lib/presentation/root/pages/root.dart
+++ lib/presentation/root/pages/root.dart
@@ -1,12 +1,10 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class RootPage extends StatelessWidget {
   const RootPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold();
   }
 }
