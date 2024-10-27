7f5f0271b732682f661fcff7ce4a22d22ff55fc9
hpzynha <larissa.nogueira.rocha@gmail.com>
2024-10-28 08:12:52 +1030

    tabs

--- lib/presentation/home/pages/home.dart
+++ lib/presentation/home/pages/home.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:spotify_app_clone/common/helpers/is_dark_mode.dart';
 import 'package:spotify_app_clone/core/configs/assets/app_images.dart';
+import 'package:spotify_app_clone/core/configs/theme/app_colors.dart';
 
 import '../../../common/appbar/app_bar.dart';
 import '../../../core/configs/assets/app_vectors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 4, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,33 +38,68 @@ class HomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [_homeTopCard()],
+          children: [
+            _homeTopCard(),
+            _tabs(),
+          ],
         ),
       ),
     );
   }
 
   Widget _homeTopCard() {
-    return Center(
+    return Padding(
+      padding: const EdgeInsets.only(left: 20.0),
       child: SizedBox(
         height: 140,
         child: Stack(
           children: [
             Align(
-                alignment: Alignment.bottomCenter,
-                child: SvgPicture.asset(AppVectors.homeCard)),
+              alignment: Alignment.bottomCenter,
+              child: SvgPicture.asset(
+                AppVectors.homeCard,
+              ),
+            ),
             Align(
               alignment: Alignment.bottomRight,
               child: Padding(
-                padding: const EdgeInsets.only(
-                  right: 60,
-                ),
+                padding: const EdgeInsets.only(right: 60),
                 child: Image.asset(AppImages.homeArtist),
               ),
-            ),
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _tabs() {
+    return TabBar(
+      controller: _tabController,
+      isScrollable: true,
+      labelColor: context.isDarkMode ? Colors.white : Colors.black,
+      indicatorColor: AppColors.primary,
+      padding: const EdgeInsets.symmetric(
+        vertical: 40,
+      ),
+      tabs: const [
+        Text(
+          'News',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        Text(
+          'Videos',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        Text(
+          'Artists',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        ),
+        Text(
+          'Podcasts',
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+        )
+      ],
     );
   }
 }
